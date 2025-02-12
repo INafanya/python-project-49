32fbae0df36dae910d69cf0aa2d22261b19816bd
Andrey Solovyov <solovyov.net@gmail.com>
2025-02-12 19:57:50 +0300

    fix formating

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,12 +6,12 @@ from brain_games.is_even_logic import get_randint, is_even
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'''Hello, {name}!\n
-          Answer "yes" if the number is even, otherwise answer "no".''')
+    print(f'Hello, {name}!\n')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     attempt = 0
     while attempt < 3:
         number = get_randint()
-        even = is_even()
+        even = is_even(number)
         print(f'Question: {number}')
         user_answer = prompt.string('Your answer: ')
         if user_answer == even:
@@ -21,9 +21,10 @@ def main():
                 print(f'Congratulations, {name}!')
                 break
         else:
-            print(f"""'{user_answer}' is wrong answer ;(
-                  . Correct answer was '{even}'.\n
-                  Let's try again, {name}!""")
+            print(f"'{user_answer}' is wrong answer ;(\
+. Correct answer was '{even}'.")
+            print("Let's try again, {name}!")
+            
             break
 
 
